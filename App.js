ff27682f29575e85f186aa4b58433036292d25d8
OliverJack0 <caue_oliveira_@hotmail.com>
2024-03-05 06:17:03 -0300

    App_pr

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Key from './SRC/components/KeyBord'
+import Scroll from './SRC/components/ScrollView'
+export default function App() {
+  return (
+    <ScrollView>
+    <View>
+     <Key/>
+    </View>
+    </ScrollView>
+    
+  );
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
